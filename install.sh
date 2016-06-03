82e5734e66115dd216a82d11e99847f357258a89
Anthony Lombardi <alombardi@tmpg.com>
2016-06-03 19:56:52 -0400

    fixed shell url

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 
 echo "Installing slack_cleaner"
-git clone --depth=1 https://github.com/skwp/dotfiles.git "$HOME/.slack_cleaner"
+git clone --depth=1 https://raw.githubusercontent.com/t0nylombardi/Slack_cleaner/master/install.sh "$HOME/.slack_cleaner"
 
 rake install
