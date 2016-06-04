88d8b93539877ba06835ff08d730673c0b4a1b15
Anthony Lombardi <alombardi@tmpg.com>
2016-06-03 20:04:36 -0400

    adding updater

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 
 
 echo "Installing slack_cleaner"
-git clone --depth=1 https://raw.githubusercontent.com/t0nylombardi/Slack_cleaner/master/install.sh "$HOME/.slack_cleaner"
 
 rake install
+
+if [ ! -d "$HOME/.slack_cleaner" ]; then
+    echo "Installing YADR for the first time"
+    git clone --depth=1 https://raw.githubusercontent.com/t0nylombardi/Slack_cleaner/master/install.sh "$HOME/.slack_cleaner"
+    cd "$HOME/.slack_cleaner"
+    rake install
+else
+    echo "updating slack_cleaner"
+    git pull
+fi
