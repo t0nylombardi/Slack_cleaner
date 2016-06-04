2286a838e2fe3533a9506c0556726ae4ccd8f8e9
Anthony Lombardi <alombardi@tmpg.com>
2016-06-04 18:39:58 -0400

    fixes to install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-
-echo "Installing slack_cleaner"
-
-rake install
-
 if [ ! -d "$HOME/.slack_cleaner" ]; then
     echo "Installing YADR for the first time"
     git clone --depth=1 https://github.com/t0nylombardi/Slack_cleaner.git "$HOME/.slack_cleaner"
@@ -14,4 +9,5 @@ else
     echo "updating slack_cleaner"
     cd "$HOME/.slack_cleaner"
     git pull
+    rake install
 fi
